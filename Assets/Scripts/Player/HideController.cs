673b3f7ce3aaa219328610cf336886acc1d8e62b
N[i]kolasPolas <petruchok02@mail.ru>
2023-05-31 10:53:23 +0300

    Add shelter

---
+++ Assets/Scripts/Player/HideController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideController : MonoBehaviour
+{
+    [SerializeField] private Transform _leftHideBorder;
+    [SerializeField] private Transform _rightHideBorder;
+    [SerializeField] private float _radius;
+    public bool IsHidden { get; private set; }
+    private void Update()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        if (_leftHideBorder && _rightHideBorder)
+        {
+            Gizmos.DrawWireSphere(_leftHideBorder.position, _radius);
+            Gizmos.DrawWireSphere(_rightHideBorder.position, _radius);
+        }
+    }
+}
