abc2836d9dc52c45a51f85d5710dd34f08e47e0a
artemoshepkov <artemoshepkov790@gmail.com>
2023-06-05 19:52:52 +0700

    Add camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _followto;
+    [SerializeField] [Range(1, 10)] private float _smoothingCoef;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private Vector3 _minValues;
+    [SerializeField] private Vector3 _maxValues;
+
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        var followToPosition = _followto.position + _offset;
+
+        var boundPosition = new Vector3(
+            Mathf.Clamp(followToPosition.x, _minValues.x, _maxValues.x),
+            Mathf.Clamp(followToPosition.y, _minValues.y, _maxValues.y),
+            Mathf.Clamp(followToPosition.z, _minValues.z, _maxValues.z));
+
+        var smoothedPosition = Vector3.Lerp(transform.position, boundPosition, _smoothingCoef * Time.fixedDeltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
